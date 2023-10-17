e21201b00d228eac6c9c4ad176e304a2be5c153f
Mohammad Fathi <mohfabdou@gmail.com>
2023-10-17 10:36:08 +0200

    saving changes

--- fc_handler.c
+++ fc_handler.c
@@ -12,17 +12,43 @@
  */
 int nums_convs(const char* format, unsigned int fc_ptr, va_list *list)
 {
+	unsigned int i = fc_ptr;
+	int l = 0, h = 0;
+
+	while (format[i] != '%')
+	{
+		if (format[i] == 'l')
+			l = 0;
+		else if (format[i] == 'h')
+			h = 0;
+		i--;
+	}
 	switch (format[fc_ptr])
 	{
 		case 'd':
 		case 'i':
-			return (fc_d_i(va_arg(*list, int), fc_ptr, format));
+			if (l)
+				return (fc_d_i(va_arg(*list, long int), fc_ptr, format));
+			else if (h)
+				return (fc_d_i(va_arg(*list, short int), fc_ptr, format));
+			else
+				return (fc_d_i(va_arg(*list, int), fc_ptr, format));
 			break;
 		case 'u':
-			return (fc_u(va_arg(*list, unsigned int)));
+			if (l)
+				return (fc_u(va_arg(*list, unsigned long int)));
+			else if (h)
+				return (fc_u(va_arg(*list, unsigned short int)));
+			else
+				return (fc_u(va_arg(*list, unsigned int)));
 			break;
 		case 'o':
-			return (fc_o(va_arg(*list, unsigned int), fc_ptr, format));
+			if (l)
+				return (fc_o(va_arg(*list, unsigned long int), fc_ptr, format));
+			else if (h)
+				return (fc_o(va_arg(*list, unsigned short int), fc_ptr, format));
+			else
+				return (fc_o(va_arg(*list, unsigned int), fc_ptr, format));
 			break;
 		case 'x':
 			return (fc_x(va_arg(*list, unsigned int), 0, fc_ptr, format));
